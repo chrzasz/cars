2b140df90e26402f22f0cd0596cf5bf37ec32944
Greg <chrzaszczyk@gmail.com>
2019-10-06 15:52:11 +0200

    UI ver1

--- src/main/java/pl/inome/cars/ui/Main.java
+++ src/main/java/pl/inome/cars/ui/Main.java
@@ -1,104 +1,151 @@
 package pl.inome.cars.ui;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.HeaderRow;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.provider.DataProvider;
-import com.vaadin.flow.data.provider.ListDataProvider;
-import com.vaadin.flow.data.provider.SortDirection;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import org.apache.commons.lang3.StringUtils;
 import pl.inome.cars.model.Car;
 import pl.inome.cars.model.CarColor;
 import pl.inome.cars.model.CarMark;
 import pl.inome.cars.service.CarService;
 import pl.inome.cars.utils.CustomConverter;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.EnumSet;
 
 @Route
 public class Main extends VerticalLayout {
 
     private CarService carService;
     private Grid<Car> carGrid;
-    private AtomicReference<ListDataProvider<Car>> carListDataProvider;
-    private CarEditForm carEditForm;
-    private CustomConverter converter;
+    private TextField carModelTextField;
     private ComboBox<CarMark> carMarkComboBox;
     private ComboBox<CarColor> carColorComboBox;
+    private Button saveButton;
+    private Button deleteButton;
+    private Button addButton;
+    private Notification notification;
+    private CustomConverter converter;
+    private Car tmpCar;
+
 
     public Main(CarService carService) {
-
         this.carService = carService;
         converter = new CustomConverter();
+        tmpCar = new Car();
+
+        // Create the fields
+        notification = new Notification();
+        notification.setDuration(1500);
+        carModelTextField = new TextField("Model");
+        carModelTextField.setPlaceholder("Model of the car");
+        carModelTextField.setValueChangeMode(ValueChangeMode.EAGER);
+        carMarkComboBox = new ComboBox<>("Mark");
+        carMarkComboBox.setItems(EnumSet.allOf(CarMark.class));
+        carColorComboBox = new ComboBox<>("Color");
+        carColorComboBox.setItems(EnumSet.allOf(CarColor.class));
+        saveButton = new Button("Save", VaadinIcon.CHECK.create());
+        deleteButton = new Button("Delete", VaadinIcon.TRASH.create());
+        addButton = new Button("Add Car", VaadinIcon.PLUS.create());
+
+        // Edit fields bar
+        HorizontalLayout editFieldsLayout = new HorizontalLayout();
+        editFieldsLayout.add(carMarkComboBox, carModelTextField, carColorComboBox);
+
+        // Button bar
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonsLayout.add(addButton, saveButton, deleteButton);
+        addButton.getStyle().set("mariginRight", "10px");
+        saveButton.getStyle().set("mariginRight", "10px");
+        enableButtons(false);
+
+        // Grid
         carGrid = new Grid<>();
         carGrid.setHeightByRows(true);
+        refreshGrid();
+        carGrid.addColumn(Car::getId).setHeader("ID");
+        carGrid.addColumn(Car::getMark).setHeader("Mark");
+        carGrid.addColumn(Car::getModel).setHeader("Model");
+        carGrid.addColumn(Car::getColor).setHeader("Color");
 
-        carListDataProvider = new AtomicReference<>(
-                new ListDataProvider<>(converter.getListFromIteralbe(carService.getAllCars())));
-        carGrid.setDataProvider(carListDataProvider.get());
-
-        Grid.Column<Car> markColumn = carGrid
-                .addColumn(Car::getMark)
-                .setHeader("Mark");
-        Grid.Column<Car> modelColumn = carGrid
-                .addColumn(Car::getModel)
-                .setHeader("Model");
-        Grid.Column<Car> colorColumn = carGrid
-                .addColumn(Car::getColor)
-                .setHeader("Color");
-
-        HeaderRow filterRow = carGrid.appendHeaderRow();
-        // First filter
-        carMarkComboBox = new ComboBox<>();
-        carMarkComboBox.setItems(CarMark.values());
-        carMarkComboBox.addValueChangeListener(e -> applyFilter(carListDataProvider.get()));
-        filterRow.getCell(markColumn).setComponent(carMarkComboBox);
-        carMarkComboBox.setSizeFull();
-        // Second filter
-        TextField modelTextField = new TextField();
-        modelTextField.addValueChangeListener(e -> carListDataProvider.get().addFilter(
-                car -> StringUtils.containsIgnoreCase(car.getModel(),
-                        modelTextField.getValue())));
-        modelTextField.setValueChangeMode(ValueChangeMode.EAGER);
-        filterRow.getCell(modelColumn).setComponent(modelTextField);
-        modelTextField.setSizeFull();
-        modelTextField.setPlaceholder("Filter");
-        // Third filter
-        carColorComboBox = new ComboBox<>();
-        carColorComboBox.setItems(CarColor.values());
-        carColorComboBox.addValueChangeListener(e -> applyFilter(carListDataProvider.get()));
-        filterRow.getCell(colorColumn).setComponent(carColorComboBox);
-        carColorComboBox.setSizeFull();
-
-        add(carGrid);
-
-        carEditForm = new CarEditForm(carService);
-
-        carListDataProvider.set(DataProvider.ofCollection(converter.getCollectionFromIteralbe(carService.getAllCars())));
-        carListDataProvider.get().setSortOrder(Car::getMark, SortDirection.ASCENDING);
-        carGrid.setDataProvider(carListDataProvider.get());
+        // Listeners
+        addButton.addClickListener(e -> addCar());
+        saveButton.addClickListener(e -> saveCar(tmpCar));
+        deleteButton.addClickListener(e -> deleteCar(tmpCar));
 
         carGrid.addItemClickListener(e -> {
-            carListDataProvider.set(DataProvider.ofCollection(converter.getCollectionFromIteralbe(carService.getAllCars())));
-            carListDataProvider.get().setSortOrder(Car::getMark, SortDirection.ASCENDING);
-            carGrid.setDataProvider(carListDataProvider.get());
-
-            carGrid.getDataProvider().refreshAll();
-            carEditForm.setCar(e.getItem());
-            add(carEditForm);
+            enableButtons(true);
+            tmpCar = e.getItem();
+            carMarkComboBox.setValue(tmpCar.getMark());
+            carModelTextField.setValue(tmpCar.getModel());
+            carColorComboBox.setValue(tmpCar.getColor());
+            carModelTextField.focus();
         });
 
+        // add fields to layout
+        add(editFieldsLayout);
+        add(buttonsLayout);
+        add(carGrid);
+
     }
 
-    private void applyFilter(ListDataProvider<Car> dataProvider) {
-        dataProvider.clearFilters();
-        if (carMarkComboBox.getValue() != null)
-            dataProvider.addFilter(car -> carMarkComboBox.getValue() == car.getMark());
-        if (carColorComboBox.getValue() != null)
-            dataProvider.addFilter(car -> carColorComboBox.getValue() == car.getColor());
+    private void saveCar(Car car) {
+        if ((carMarkComboBox.getValue() != null) &&
+                (!carModelTextField.getValue().isEmpty()) &&
+                (carColorComboBox.getValue() != null)) {
+            car.setMark(carMarkComboBox.getValue());
+            car.setModel(carModelTextField.getValue());
+            car.setColor(carColorComboBox.getValue());
+            carService.updateCar(car.getId(), car);
+            refreshGrid();
+            enableButtons(false);
+        } else {
+            notification.setText("Not saved. Select all fields.");
+            notification.open();
+        }
     }
+
+    private void deleteCar(Car car) {
+        carService.deleteCar(car.getId());
+        enableButtons(false);
+        refreshGrid();
+    }
+
+
+    private void addCar() {
+        Car car = new Car();
+        if ((carMarkComboBox.getValue() != null) &&
+                (!carModelTextField.getValue().isEmpty()) &&
+                (carColorComboBox.getValue() != null)) {
+            car.setMark(carMarkComboBox.getValue());
+            car.setModel(carModelTextField.getValue());
+            car.setColor(carColorComboBox.getValue());
+            carService.addCar(car);
+            refreshGrid();
+        } else {
+            notification.setText("Not added. Select all required fields.");
+            notification.open();
+        }
+    }
+
+    private void refreshGrid() {
+        carGrid.setItems(converter.getListFromIteralbe(carService.getAllCars()));
+        carGrid.getDataProvider().refreshAll();
+    }
+
+    private void enableButtons(boolean enable) {
+        saveButton.setEnabled(enable);
+        deleteButton.setEnabled(enable);
+        addButton.setEnabled(!enable);
+    }
+
+
 }
