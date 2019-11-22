48f0e2ae20ec3c87378554db98a03617df02a23b
Greg <chrzaszczyk@gmail.com>
2019-11-22 14:25:38 +0100

    refactoring Repo

--- src/main/java/pl/inome/cars/repository/CarRepository.java
+++ src/main/java/pl/inome/cars/repository/CarRepository.java
@@ -1,6 +1,5 @@
 package pl.inome.cars.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.inome.cars.model.Car;
 import pl.inome.cars.model.CarColor;
@@ -12,7 +11,6 @@ public interface CarRepository extends CrudRepository<Car, Long> {
 
     Iterable<Car> findByMark(CarMark mark);
 
-    //    @Query("select c from Car c where lower(c.model) like lower(concat('%', :model, '%') )")
     Iterable<Car> findByModelIsContainingIgnoreCase(String model);
 
     Iterable<Car> findByProductionYearIsContaining(String year);
@@ -21,18 +19,6 @@ public interface CarRepository extends CrudRepository<Car, Long> {
 
     Iterable<Car> findByProductionYearIsLessThanEqual(String year);
 
-//    @Query(value = "SELECT * FROM car WHERE car.production_year >= :min AND car.production_year <= :max", nativeQuery = true)
-//    Iterable<Car> findByYearRange(@Param("min") String min, @Param("max") String max);
-
     Iterable<Car> findByProductionYearBetweenOrderByProductionYearDesc(String min, String max);
-
-    @Query("select c from Car c where " +
-            "c.mark like :carMark and " +
-            "lower(c.model) like lower(concat('%', :model, '%') ) and " +
-            "c.color like :color and " +
-            "c.productionYear>= :yearMin and " +
-            "c.productionYear<=:yearMax")
-    Iterable<Car> filter(CarMark carMark, String model, CarColor color, String yearMin, String yearMax);
-
 
 }
